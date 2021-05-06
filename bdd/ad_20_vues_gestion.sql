ee149e87115b3cd9200ff58772c991bab74bce10
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-05-06 10:11:43 +0200

    Update ad_20_vues_gestion.sql

--- bdd/ad_20_vues_gestion.sql
+++ bdd/ad_20_vues_gestion.sql
@@ -1,3 +1,10 @@
+/*ADRESSE V1.0*/
+/*Creation des vues et triggers nécessaires à la gestion via l'application web-métier */
+/*ad_20_VUES_GESTION.sql */
+/*PostGIS*/
+/* GeoCompiegnois - http://geo.compiegnois.fr/ */
+/* Auteur : Grégory Bodet*/
+
 -- ####################################################################################################################################################
 -- ###                                                                                                                                              ###
 -- ###                                                                        VUES                                                                  ###
