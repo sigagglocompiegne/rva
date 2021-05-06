fe20a9251939ee9f6fbacf0c97c28530b51f0c06
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-05-06 10:03:06 +0200

    Update voie_00_trace.sql

--- bdd/voie_00_trace.sql
+++ bdd/voie_00_trace.sql
@@ -1,1 +1,8 @@
-test
+-- #################################################################### SUIVI CODE SQL ####################################################################
+
+-- 2016/05/20 : FV / initialisation du code
+-- 2018/04/12 : GB / Intégration des modifications sur les vues applicatives pour l'affichage côté utilisateur dans GEO
+-- 2018/08/07 : GB / Insertion des nouveaux rôles de connexion et leurs privilèges
+-- 2021/02/16 : GB / Modification des fonctions triggers de la vue de gestion et de la classe an_voie pour mieux vérifier la saisie des codes RIVOLI issus du FANTOIR et gérer les RIVOLI provisoires
+-- 2021/03/15 : GB / Modification des droits
+
