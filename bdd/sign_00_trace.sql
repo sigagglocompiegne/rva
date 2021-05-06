5674c83f4c084e3461d68209f58861a180498f3e
gregbodet <37414914+gregbodet@users.noreply.github.com>
2021-05-06 11:18:28 +0200

    Update sign_00_trace.sql

--- bdd/sign_00_trace.sql
+++ bdd/sign_00_trace.sql
@@ -1,1 +1,7 @@
-test
+-- #################################################################### SUIVI CODE SQL ####################################################################
+
+-- 2017/01/12 : FV / initialisation du code pour une donnée de signalement sur le Référentiel des Voies et Adresses (RVA)
+-- 2018/03/20 : GB / Intégration des modifications suite au groupe de travail RVA du 13 mars 2018
+-- 2018/08/07 : GB / Intégration des nouveaux rôles de connexionss et leurs privilèges
+-- 2020/09/23 : GB / Modification de la gestion des suppression des signalements (possible à partir de la vue de gestion à travers le projet QGIS mais pas à travers l'application WEB)
+
