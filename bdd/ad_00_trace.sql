c338938ba07feae5b4f52019cc1a73ca4cab444c
gregbodet <37414914+gregbodet@users.noreply.github.com>
2023-08-23 08:26:39 +0200

    Update ad_00_trace.sql

--- bdd/ad_00_trace.sql
+++ bdd/ad_00_trace.sql
@@ -50,3 +50,4 @@
 -- 2021/12/08 : GB / Modification trigger gestion des adresses (ajout d'un contrôle sur la suppressio impossible d'un point d'adresse relié à un établissement
 -- 2022/10/27 : GB / Suppression du diagnostic adresse, pour les adresses non numérotée en projet, reporté sur adresse non numérotée avec un état d'avancement à non commencé
 -- 2023/07/20 : GB / Intéfration de l'identifiant BAN au format UUIDV4
+-- 2023/08/23 : GB / Ajout dans la fonction de gestion des adresses, l'impossibilité de suppirmer une adresse si elle est utilisée par le SPANC ou l'ANC
